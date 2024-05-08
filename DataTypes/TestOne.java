d43c41d87777458497f8336f18b61e874552fa1c
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-05-08 20:08:43 +0530

    test

---
+++ DataTypes/TestOne.java
@@ -0,0 +1,14 @@
+package DataTypes;
+
+public class TestOne{
+    public static void main(String[] args) {
+
+        //create variable
+        //range-- -128 to 127
+
+       // byte b=129;
+       byte b=125;
+        System.out.println(b);
+    }
+    }
+
