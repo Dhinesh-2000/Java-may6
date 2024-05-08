f8c4bacc1f7cc282774d601176761ec13dc1df6d
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-05-08 19:18:38 +0530

    test

--- Identifiers/Test.java
+++ Identifiers/Test.java
@@ -1,9 +1,8 @@
-public Class Test{
-
+public class Test {    
     int eid=101;
 
     String ename="Rahul Gadndhi";
     public void getDetails(){}
     public void true1(){}
     String String ="Rahul";
-}
+} 
