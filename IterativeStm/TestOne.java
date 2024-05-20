155c458a9c9b48a102e40528a89cf9fc74a1be9a
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-05-20 19:05:51 +0530

    test

---
+++ IterativeStm/TestOne.java
@@ -0,0 +1,14 @@
+//tp print the values from 1 t0 5 using for 
+
+/*
+ * ini st  : i=1;
+ * cond st: i<=5;
+ * inc / dec st : i++ / i--
+ */
+class Test{
+public static void main(String[] args) {
+    for(int i=1;i<=5;i++){
+        System.out.println(i);
+    }
+}
+}
