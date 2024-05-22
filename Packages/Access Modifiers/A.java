bbb42fff12bd9a3ba0b178840599a549c9d5e00f
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-05-22 16:04:15 +0530

    test

---
+++ Packages/Access Modifiers/A.java
@@ -0,0 +1,15 @@
+package pack1;
+
+public class A {
+    public void m1()
+    {
+        System.out.println("public class -A -public method");
+    }
+
+    void m2(){
+        System.out.println("public class -A - default method");
+    }
+
+
+    
+} 
