1fe56b1ea2b0ad30692e03b585d53de93db8d1c9
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-11-14 19:58:56 +0530

    test

---
+++ New-Java/Basics/Arithmetic_Op.java
@@ -0,0 +1,21 @@
+public class Arithmetic_Op {
+    public static void main(String[] args) {
+        int num1=10;
+        //int num2=20;
+         //int re = num1 + num2;
+         //num1 += 2;
+         //num1 -= 2;
+         //num1 *= 2;
+         //num1 /= 2;
+         //num1 %= 2;
+
+         //num1++;  //post-increment
+          
+         //num1--;   //post-decrement
+
+         //++num1;     //pre-increment
+         --num1;     //pre-decrement
+    
+        System.out.println(num1);
+    }
+}
---
+++ New-Java/Basics/Datatypes.java
@@ -0,0 +1,21 @@
+class Datatypes{
+    public static void main(String[] args) {
+        int num1 = 9;
+        byte b = 127;
+        short sh = 558;
+        long l = 5656l;
+
+        float f =5.8f;
+        double d=5.8;
+
+
+        char c= 'u';
+        boolean bu =true;
+       
+        float fs=4.5f;
+        int x=(int)fs;
+        System.out.println(x);
+       
+
+    }
+}
