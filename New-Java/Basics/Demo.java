56e2635d32d81e6e5a90aff36c9ba47687bd1da3
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-11-17 15:57:29 +0530

    test

---
+++ New-Java/Basics/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+   public static void main(String[] args) {
+    int x=5;
+    int y=8;
+
+    int a=4;
+    int b=7;
+    boolean result = x<y || a>b;
+    System.out.println(result);
+   } 
+}
