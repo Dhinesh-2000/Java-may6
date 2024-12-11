707830031b6a070a13346b7e79bf8d66ca5b61ba
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-12-11 20:52:10 +0530

    tst

---
+++ New-Java/Basics/DemoWhile.java
@@ -0,0 +1,16 @@
+public class DemoWhile {
+    public static void main(String[] args) {
+        // int i =6;
+        // do{
+        //     System.out.println("Hi" + i);
+        //     i++;
+        // }while (i<=5);
+       // // System.out.println("Bye " + 1);
+    for(int i=1;i<=5;i++){
+       System.out.println("DAY " + i);
+       for(int j=1;j<=9;j++){
+        System.out.println(" " + (j + 8) + " - "+ (j+9));
+       }
+    }   
+    }
+}
